b1daf0da03aee6533e13fab279aa63871cad44d8
Gustavo Galupo <ggalupo2@hotmail.com>
2021-04-20 01:21:23 -0300

    MainHeader styles, structure, emoji logic.

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
-import { Container } from './styles'
+import emojis from '../../utils/emojis'
+
+import { Container, Profile, Welcome, UserName } from './styles'
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const index = Math.floor(Math.random() * emojis.length)
+        return emojis[index]
+    }, [])
+
     return (
         <Container>
-            <h1>MainHeader</h1>
+            <h1>Toggle</h1>
+
+            <Profile>
+                <Welcome>Hello, {emoji}</Welcome>
+                <UserName>Gustavo</UserName>
+            </Profile>
         </Container>
     )
 }
