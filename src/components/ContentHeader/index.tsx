0a8b8387eeff8f73b77f5964f7fd69c634cff456
Gustavo Galupo <ggalupo2@hotmail.com>
2021-04-20 16:02:27 -0300

    Content header styles and title.

---
+++ src/components/ContentHeader/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Container, TitleContainer, Controllers } from './styles'
+
+const ContentHeader: React.FC = () => {
+    return (
+        <Container>
+            <TitleContainer>
+                <h1>Título</h1>
+            </TitleContainer>
+            <Controllers>
+                <button type="button"> Button A</button>
+                <button type="button"> Button B</button>
+            </Controllers>
+        </Container>
+    )
+}
+
+export default ContentHeader
