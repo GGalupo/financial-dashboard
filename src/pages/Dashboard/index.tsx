879a0a2c007a25bf4d1e5ffca390e8fa400bc623
Gustavo Galupo <ggalupo2@hotmail.com>
2021-04-20 21:36:47 -0300

    ContentHeader selects and dynamic title for pages.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput'
+
 import { Container } from './styles'
 
 const Dashboard: React.FC = () => {
+
+    const options = [
+        {value: 'Gustavo', label: 'Gustavo'},
+        {value: 'Carol', label: 'Carol'},
+        {value: 'Ana', label: 'Ana'}
+    ]
+
     return (
         <Container>
-            <ContentHeader />
+            <ContentHeader title="Dashboard" lineColor="#F7931B">
+                <SelectInput options={options}/>
+            </ContentHeader>
         </Container>
     )
 }
