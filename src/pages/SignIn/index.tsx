662778a885ee92c55c52934d3ee5d02e514d47de
Gustavo Galupo <ggalupo2@hotmail.com>
2021-05-03 20:29:40 -0300

    Login page styles and components.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,62 @@
 import React from 'react'
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { FaGithub, FaInstagram } from 'react-icons/fa'
+import { AiFillLinkedin, AiOutlineDollar } from 'react-icons/ai'
+
+import {
+    Container,
+    LogoContainer,
+    LoginContainer,
+    Form,
+    SocialIconsContainer,
+    SocialIcon
+} from './styles'
+
 const SignIn: React.FC = () => {
     return (
-        <h1>SignIn</h1>
+        <Container>
+            <LogoContainer>
+                <AiOutlineDollar />
+            </LogoContainer>
+            <LoginContainer>
+                <Form onSubmit={() => {}}>
+                    <h2>Login</h2>
+                    <Input
+                        required
+                        placeholder="username"
+                    />
+                    <Input
+                        required
+                        type="password"
+                        placeholder="password"
+                    />
+                <Button type="submit">Login</Button>
+                </Form>
+                <SocialIconsContainer>
+                    <SocialIcon
+                        href="https://github.com/GGalupo"
+                        target="_blank"
+                    >
+                        <FaGithub />
+                    </SocialIcon>
+                    <SocialIcon
+                        href="https://www.linkedin.com/in/ggalupo/"
+                        target="_blank"
+                    >
+                        <FaInstagram />
+                    </SocialIcon>
+                    <SocialIcon
+                        href="https://instagram.com/ggalupo"
+                        target="_blank"
+                    >
+                        <AiFillLinkedin />
+                    </SocialIcon>
+                </SocialIconsContainer>
+            </LoginContainer>
+        </Container>
     )
 }
 
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    height: 100vh;
+    background-color: ${props => props.theme.colors.primary};
+
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+
+`
+
+export const LogoContainer = styled.div`
+    margin-bottom: 20px;
+
+    > svg {
+        color: #5f9341;
+
+        width: 80px;
+        height: 80px;
+    }
+
+`
+
+export const LoginContainer = styled.div`
+    width: 300px;
+    height: 340px;
+    
+    padding: 25px 30px;
+
+    border-radius: 10px;
+
+    display: flex;
+    flex-direction: column;
+
+    background-color: ${props => props.theme.colors.tertiary};
+`
+
+export const Form = styled.form`
+
+    > h2 {
+        color: ${props => props.theme.colors.white};
+        margin-bottom: 20px;
+
+        &:after {
+            content: '';
+            display: block;
+            width: 50px;
+            margin-right: auto;
+            margin-top: 1px;
+            border-bottom: 9px solid ${props => props.theme.colors.warning}
+        }
+    }
+`
+
+export const SocialIconsContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+    width: 100%;
+
+    margin-top: 25px;
+`
+
+export const SocialIcon = styled.a`
+
+    > svg {
+        width: 40px;
+        height: 40px;
+
+        color: ${props => props.theme.colors.white};
+
+        &:hover {
+            transition: opacity .2s;
+            opacity: .6;
+        }
+    }
+`
